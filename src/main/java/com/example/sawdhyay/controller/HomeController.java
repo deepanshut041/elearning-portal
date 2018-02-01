31b0a05085da62526fb19734b4c3f43014e14206
deepanshut041 <deepanshut041@gmail.com>
2018-02-02 01:58:30 +0530

    Made header designs now working on footer

--- src/main/java/com/example/sawdhyay/controller/HomeController.java
+++ src/main/java/com/example/sawdhyay/controller/HomeController.java
@@ -2,7 +2,6 @@ package com.example.sawdhyay.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
