9cd004b326b35891ef84e58705bed8fb1c74e1e7
deepanshut041 <deepanshut041@gmail.com>
2018-02-07 18:10:31 +0530

    Made a simple course controller now working on other controller

---
+++ src/main/java/com/example/sawdhyay/controller/CourseController.java
@@ -0,0 +1,21 @@
+package com.example.sawdhyay.controller;
+
+import com.example.sawdhyay.repositories.CourseRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CourseController {
+
+    private CourseRepository courseRepository;
+    public CourseController(CourseRepository courseRepository){
+        this.courseRepository = courseRepository;
+    }
+
+    @RequestMapping("/courses")
+    public String getCourses(Model model){
+        model.addAttribute("courses", courseRepository.findAll());
+        return "courses";
+    }
+}
