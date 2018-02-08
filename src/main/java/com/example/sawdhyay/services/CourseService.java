a8e895117fa2de1a15c63ac830091db81aa74b8a
deepanshut041 <deepanshut041@gmail.com>
2018-02-09 03:13:49 +0530

    Made courseservice now working on its implemantion and dao

---
+++ src/main/java/com/example/sawdhyay/services/CourseService.java
@@ -0,0 +1,27 @@
+package com.example.sawdhyay.services;
+
+import com.example.sawdhyay.models.Course;
+
+import java.util.List;
+
+public interface CourseService {
+    Course findById(int id);
+
+    void saveCourse(Course course);
+
+    void updateCourse(Course course);
+
+    void deleteCourseById(int id);
+
+    List<Course> findAllCourses();
+
+    Course findCourseById(int id);
+
+    List<Course> findCoursesByCategory(int category);
+
+    List<Course> findCoursesByTechLanguage(int tech_language_id);
+
+    List<Course> findCoursesByMentor(int mentor_id);
+
+
+}
