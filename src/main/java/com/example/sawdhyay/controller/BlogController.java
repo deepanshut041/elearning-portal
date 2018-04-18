69af85816ec00052b288967ef7439df7431d129d
deepanshut041 <deepanshut041@gmail.com>
2018-04-18 10:36:38 +0530

    User details left working on it

--- src/main/java/com/example/sawdhyay/controller/BlogController.java
+++ src/main/java/com/example/sawdhyay/controller/BlogController.java
@@ -1,6 +1,9 @@
 package com.example.sawdhyay.controller;
 
 
+import com.example.sawdhyay.models.Post;
+import com.example.sawdhyay.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,14 +11,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/blogs")
 public class BlogController {
 
+    @Autowired
+    private PostService postService;
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView blogsPage(Model model){
         ModelAndView modelAndView = new ModelAndView();
+        List<Post> posts = postService.findAllPosts();
+        modelAndView.addObject("posts", posts);
         modelAndView.setViewName("blogs");
         return modelAndView;
     }
